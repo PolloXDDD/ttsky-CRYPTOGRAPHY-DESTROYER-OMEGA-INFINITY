5290c5ffe77f5de96fa6d81ebefef5de50f64bd5
Kaoru Aguilera Katayama <elpollitocientifico@gmail.com>
2026-09-01 19:17:00 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,9 +1,50 @@
-// Modelo de simulación para que la malla física entregue potenciales reales en el testbench
+`default_nettype none
+`timescale 1ns / 1ps
+
+module tb ();
+
+  initial begin
+    $dumpfile("tb.fst");
+    $dumpvars(0, tb);
+    #1;
+  end
+
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
+
+  tt_um_omega_infinity_kaoru user_project (
+`ifdef GL_TEST
+      .VPWR   (VPWR),
+      .VGND   (VGND),
+`endif
+      .ui_in  (ui_in),
+      .uo_out (uo_out),
+      .uio_in (uio_in),
+      .uio_out(uio_out),
+      .uio_oe (uio_oe),
+      .ena    (ena),
+      .clk    (clk),
+      .rst_n  (rst_n)
+  );
+
+endmodule
+
+// Stubs de simulación para que iverilog no falle en modo RTL
 `ifndef GL_TEST
 module omega_metal_grid_3d (
     output wire [7:0] taps
 );
-    // Asignación de prueba de potenciales físicos discretizados
     assign taps = 8'b10101101;
 endmodule
 
